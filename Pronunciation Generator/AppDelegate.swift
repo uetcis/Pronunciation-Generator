015e27321e58a4e9f6f8b569ff3c90333169b49b
Cap.雪ノ下八幡 <CaptainYukinoshitaHachiman@ProtonMail.com>
2019-03-01 21:01:02 +0800

    Initial Commit

---
+++ Pronunciation Generator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pronunciation Generator
+//
+//  Created by CaptainYukinoshitaHachiman on 2019/3/1.
+//  Copyright © 2019 CaptainYukinoshitaHachiman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Pronunciation Generator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Pronunciation Generator
+//
+//  Created by CaptainYukinoshitaHachiman on 2019/3/1.
+//  Copyright © 2019 CaptainYukinoshitaHachiman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
